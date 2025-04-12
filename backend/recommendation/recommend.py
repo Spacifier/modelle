e9521800ce3cf9a17c8d0be724578d6043be308f
Swapnil Garg <swapnilgarg2004@gmail.com>
2025-04-12 14:11:37 +0530

    troubleshooting

--- backend/recommendation/recommend.py
+++ backend/recommendation/recommend.py
@@ -3,10 +3,10 @@ import sys
 import json
 
 # Load and preprocess the dataset
-file_path = '../recommendation/styles.csv' 
+file_path = './styles.csv' 
 data = pd.read_csv(file_path, quoting=1, on_bad_lines='skip')
 
-image_path='../recommendation/images.csv'
+image_path='./images.csv'
 image_data= pd.read_csv(image_path,on_bad_lines='skip')
 
 # Convert filename column to match id format
